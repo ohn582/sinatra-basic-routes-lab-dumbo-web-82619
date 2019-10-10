cb73cd276e86ac486a1866c1485a567be3875344
Neal JIn Oh <no4259@gmail.com>
2019-10-10 18:35:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Neal"
+  end
+  
+  get '/hometown' do
+    "My hometown is Hicksvill"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Beatles"
+  end
 end
